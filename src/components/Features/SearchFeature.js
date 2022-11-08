37842fefeeccc59f7a55ce627baff1eaf2433d88
Fabian Scherrer <fabian.scherrer@gmail.com>
2022-11-08 12:26:11 +0100

    Search Call mit EGID Gebäudesuche

--- src/components/Features/SearchFeature.js
+++ src/components/Features/SearchFeature.js
@@ -1,33 +1,44 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import classes from "./SearchFeature.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 function SearchFeature(props) {
-  const adressInputRef = useRef();
-  
-  function GetLocation(inputLocation){
-    //TODO: Adress/Location search
-    return {locAdress: inputLocation, locX: 47.5006875, locY: 8.733389};
+  const addressInputRef = useRef();
+  const [locationObject, setLocationObject] = useState({});
+
+  async function fetchAddress(inputValue) {
+    //TODO: Daten aus Input auf Search-Call mappen
+    const response = await fetch("https://api3.geo.admin.ch/rest/services/api/MapServer/find?layer=ch.bfs.gebaeude_wohnungs_register&searchText=2323872&searchField=egid&returnGeometry=false&contains=false");
+    const fetchedResponse = await response.json(response);
+    //TODO: Koordinaten 
+    setLocationObject({ inputLocation: { locAddress: inputValue, locX: 47.5006875, locY: 8.733389 }, fetch: fetchedResponse.results[0].attributes });
   };
+
+  useEffect(() => {
+    console.log(locationObject);
+
+    console.log(locationObject);
+    if (locationObject.inputLocation) {
+      const addressString = locationObject.fetch.strname_deinr + ", " + locationObject.fetch.dplz4 + " " + locationObject.fetch.ggdename;
+      props.onSearchLocation(addressString, locationObject.inputLocation.locX, locationObject.inputLocation.locY);
+    };
+
+  }, [locationObject]);
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
-    const locationObject = GetLocation(adressInputRef.current.value);
-    
-    // console.log(locationObject);
-    
-    props.onSearchLocation(locationObject.locAdress, locationObject.locX, locationObject.locY);
+    fetchAddress(addressInputRef.current.value);
   };
 
   return (
     <Card className={classes.form}>
       <form onSubmit={onSubmitHandler}>
         <input
-          id="searchAdress" 
+          id="searchAddress"
           type="text"
           placeholder="Search Location ..."
-          ref={adressInputRef}
+          ref={addressInputRef}
         />
         <Button type="submit">Search</Button>
       </form>
