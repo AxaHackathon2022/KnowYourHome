341ed49717b7c99b36eb91ceb3688ecf879e9d32
Christof Buechi <christof.buechi@gmail.com>
2022-11-09 14:29:25 +0100

    add heizungsstring

--- src/components/Features/DisplayBuildingInformationFeature.js
+++ src/components/Features/DisplayBuildingInformationFeature.js
@@ -7,11 +7,65 @@ function DisplayBuildingInformationFeature(props) {
     function getHeatingString(heatingCode) {
         let returnString;
         switch (heatingCode) {
-            case 860:
-                returnString = "Heizkessel";
+            case     7500:
+                returnString = "Keine";
                 break;
-            default:
-                returnString = "Wärmepupe/Fernwärme";
+            case     7501:
+                returnString = "Luft";
+                break;
+            case     7510:
+                returnString = "Erdwärme (generisch)";
+                break;
+            case     7511:
+                returnString = "Erdwärmesonde";
+                break;
+            case     7512:
+                returnString = "Erdregister";
+                break;
+            case     7513:
+                returnString = "Wasser (Grundwasser, Oberflächenwasser, Abwasser)";
+                break;
+            case     7520:
+                returnString = "Gas";
+                break;
+            case     7530:
+                returnString = "Heizöl";
+                break;
+            case     7540:
+                returnString = "Holz (generisch)";
+                break;
+            case     7541:
+                returnString = "Holz (Stückholz)";
+                break;
+            case     7542:
+                returnString = "Holz (Pellets)";
+                break;
+            case     7543:
+                returnString = "Holz (Schnitzel)";
+                break;
+            case     7550:
+                returnString = "Abwärme (innerhalb des Gebäudes)";
+                break;
+            case     7560:
+                returnString = "Elektrizität";
+                break;
+            case     7570:
+                returnString = "Sonne (thermisch)";
+                break;
+            case     7580:
+                returnString = "Fernwärme (generisch)";
+                break;
+            case     7581:
+                returnString = "Fernwärme (Hochtemperatur)";
+                break;
+            case     7582:
+                returnString = "Fernwärme (Niedertemperatur)";
+                break;
+            case     7598:
+                returnString = "Unbestimmt";
+                break;
+            case     7599:
+                returnString = "Andere";
                 break;
         }
         return returnString;
@@ -23,7 +77,7 @@ function DisplayBuildingInformationFeature(props) {
                 <p><b>Lat:</b> {props.currentLocation.lat}, <b>Lon:</b> {props.currentLocation.lon}</p>
                 <p>&nbsp;</p>
                 <p className={classes.displayBuildingInformation}><b>Eidgenössische Gebäude ID:</b> {props.currentBuildingInformation.egid}</p>
-                <p className={classes.displayBuildingInformation}><b>Heizung:</b> {getHeatingString(props.currentBuildingInformation.gwaersceh1)}</p>
+                <p className={classes.displayBuildingInformation}><b>Heizung:</b> {getHeatingString(props.currentBuildingInformation.genh1)}</p>
                 <p className={classes.displayBuildingInformation}><b>Baujahr:</b> {props.currentBuildingInformation.gbauj}</p>
                 <p className={classes.displayBuildingInformation}><b>Gebäudefläche:</b> {props.currentBuildingInformation.garea} &#13217;</p>
                 <p className={classes.displayBuildingInformation}><b>Anzahl Geschosse:</b> {props.currentBuildingInformation.gastw}</p>
