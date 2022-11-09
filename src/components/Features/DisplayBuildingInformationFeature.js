f0c51fe1cae780b0e7d53a04dbb8dc2d0ed0ed6d
SteelAngel13 <steelangel13@gmail.com>
2022-11-09 12:31:21 +0100

    Update Schätzung Hypo Kredit

--- src/components/Features/DisplayBuildingInformationFeature.js
+++ src/components/Features/DisplayBuildingInformationFeature.js
@@ -9,14 +9,15 @@ function DisplayBuildingInformationFeature(props) {
         {(props.currentLocation.address || props.currentBuildingInformation.egid ) && (<Card className={classes.displayBuildingInformation}>
             <h3>{props.currentLocation.address}</h3>
             <p><b>Lat:</b> {props.currentLocation.lat}, <b>Lon:</b> {props.currentLocation.lon}</p>
-            <p className={classes.displayBuildingInformation}><b>Eidgenössische Gebäude ID:</b> {props.currentBuildingInformation.egid}</p>
             <p>&nbsp;</p>
+            <p className={classes.displayBuildingInformation}><b>Eidgenössische Gebäude ID:</b> {props.currentBuildingInformation.egid}</p>            
             <p className={classes.displayBuildingInformation}><b>Heizung:</b> {props.currentBuildingInformation.gwaersceh1}</p>
             <p className={classes.displayBuildingInformation}><b>Baujahr:</b> {props.currentBuildingInformation.gbauj}</p>
             <p className={classes.displayBuildingInformation}><b>Gebäudefläche:</b> {props.currentBuildingInformation.garea} &#13217;</p>
             <p className={classes.displayBuildingInformation}><b>Gebäudevolumen:</b> {props.currentBuildingInformation.gvolsce} &#13221;</p>
             <p className={classes.displayBuildingInformation}><b>Anzahl Geschosse:</b> {props.currentBuildingInformation.gastw}</p>
-            
+            <hr/><p><b>Wert:</b> CHF 1'200'000.00 </p>
+            <hr/><p><b>Hypothek:</b> CHF 800'000.00 </p>
             
         </Card>)
         
