82e5d08d66b2d781d3cc418ae1b9f9426c985c63
chrisFinnet <50241122+chrisFinnet@users.noreply.github.com>
2022-11-09 11:37:39 +0100

    Position Display

--- src/components/Features/DisplayBuildingInformationFeature.js
+++ src/components/Features/DisplayBuildingInformationFeature.js
@@ -4,19 +4,15 @@ import Card from "../UI/Card";
 import classes from './DisplayBuildingInformation.module.css'
 
 function DisplayBuildingInformationFeature(props) {
-    console.log("BI");
-    console.log(props.currentBuildingInformation);
-    console.log(props.currentBuildingInformation.egid);
- 
-    return (
+     return (
         <>
         {(props.currentBuildingInformation ) && (<Card className={classes.displayBuildingInformation}>
-            <p>Eidgenössische Gebäude ID: {props.currentBuildingInformation.egid}</p>
-            <p>Heizung: {props.currentBuildingInformation.gwaersceh1}</p>
-            <p>Baujahr: {props.currentBuildingInformation.gbauj}</p>
-            <p>Gebäudefläche: {props.currentBuildingInformation.garea} &#13217;</p>
-            <p>Gebäudevolumen: {props.currentBuildingInformation.gvolsce} &#13221;</p>
-            <p>Anzahl Geschosse: {props.currentBuildingInformation.gastw}</p>
+            <p className={classes.displayBuildingInformation}><b>Eidgenössische Gebäude ID:</b> {props.currentBuildingInformation.egid}</p>
+            <p className={classes.displayBuildingInformation}><b>Heizung:</b> {props.currentBuildingInformation.gwaersceh1}</p>
+            <p className={classes.displayBuildingInformation}><b>Baujahr:</b> {props.currentBuildingInformation.gbauj}</p>
+            <p className={classes.displayBuildingInformation}><b>Gebäudefläche:</b> {props.currentBuildingInformation.garea} &#13217;</p>
+            <p className={classes.displayBuildingInformation}><b>Gebäudevolumen:</b> {props.currentBuildingInformation.gvolsce} &#13221;</p>
+            <p className={classes.displayBuildingInformation}><b>Anzahl Geschosse:</b> {props.currentBuildingInformation.gastw}</p>
             
             
         </Card>)
